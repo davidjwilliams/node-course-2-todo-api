3bf2d8387eb6251e8cbf9e37684dfc3fc70f2784
davidjwilliams <davidj_williams@hotmail.com>
2016-12-27 00:12:59 +0000

    Hash user password before saving

--- playgrounds/hashing.js
+++ playgrounds/hashing.js
@@ -1,14 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);	// Use jwt.io to view it
-var decoded = jwt.verify(token, '123abc'); // 123abc is our salt 
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	})
+// });
+
+var hashedPassword = '$2a$10$uAanLb65GaWXJHdIOiiVYu1M1l78I5iZm9u3QsRQhkGApAWehRm2a';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+// Correct way using jwt
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);	// Use jwt.io to view it
+// var decoded = jwt.verify(token, '123abc'); // 123abc is our salt 
+// console.log(decoded);
 
 // Manual way to do it with crypto-js
 // var message = "I am user number 3";
